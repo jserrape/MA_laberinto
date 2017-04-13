32359a76e1f3f7b9b8d7a3b31b067abef15dc118
xenahort <juan.carlos.wow.95@gmail.com>
2017-04-13 12:32:52 +0200

    -Creado el fichero de la otologia pero sin el lenguaje

---
+++ src/agentes/LaberintoOntologia.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agentes;
+
+import jade.content.onto.BeanOntology;
+import jade.content.onto.BeanOntologyException;
+import jade.content.onto.Ontology;
+
+/**
+ *
+ * @author jcsp0003
+ */
+public class LaberintoOntologia extends BeanOntology {
+
+    private static final long serialVersionUID = 1L;
+
+    // NOMBRE
+    public static final String ONTOLOGY_NAME = "Ontologia_Cuatro_en_Raya";
+
+    //VOCABULARIO
+
+
+    // The singleton instance of this ontology
+    private static Ontology INSTANCE;
+
+    public synchronized final static Ontology getInstance() throws BeanOntologyException {
+        if (INSTANCE == null) {
+            INSTANCE = new LaberintoOntologia();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Constructor
+     *
+     * @throws BeanOntologyException
+     */
+    private LaberintoOntologia() throws BeanOntologyException {
+
+        super(ONTOLOGY_NAME);
+
+        add("laberinto.elementos");
+    }
+}
